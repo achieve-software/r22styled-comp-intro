0106c94cfbc1267c4230ccbd0c529fb17649ef19
Achieve-software <vtcbcdtyr@gmail.com>
2023-07-17 14:49:07 +0300

    props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { Button } from "./components/Buton.styled";
+
 const App = () => {
   return (
     <>
-      <h1>Styled Components</h1>
+      <Button>Send</Button>
+      <Button primary>Send</Button>
     </>
   );
 };
