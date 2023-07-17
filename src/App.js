fb05fcaf3b091c3619a5de03c1742f82f23c8d89
Achieve-software <vtcbcdtyr@gmail.com>
2023-07-17 16:10:28 +0300

    links

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import { Button, FS13Button } from "./components/Buton.styled";
 import HeaderText from "./components/HeaderText";
+import Links from "./components/Links";
 import Sarmal from "./components/Sarmal";
-
 const App = () => {
   return (
     <>
-    <Sarmal bg="#bebe">
-    <HeaderText color>Styled Components</HeaderText>
-    <HeaderText color="hotpink">Styled Components</HeaderText>
-    <HeaderText color="#765234" bgrenk="#eee">Styled Components</HeaderText>
-      <Button>Send</Button>
-      <Button primary>Send</Button>
-      <FS13Button react>CALIS</FS13Button>
-      <FS13Button>DAHA ÇOK ÇALIS</FS13Button>
+      {" "}
+      <Sarmal bg="#bebe">
+        <HeaderText>Styled-components</HeaderText>
+        <HeaderText color>Styled-components</HeaderText>
+        <HeaderText color="hotpink">Styled-components</HeaderText>
+        <HeaderText
+          color="#765234
+"
+          bgrenk="#eee"
+        >
+          Styled-components
+        </HeaderText>
+        <Button>Send</Button>
+        <Button primary>Send</Button>
+        <FS13Button react>CALIS</FS13Button>
+        <FS13Button>DİNLEN</FS13Button>
+      </Sarmal>
+      <Sarmal>
+        <Links href="https://www.clarusway.com" target="_blank">
+          Clarusway Web Site
+        </Links>
+        <Links href="https://www.reactjs.org" target="_blank">
+          ReactJS
+        </Links>
+        <Links href="https://www.npm.org" target="_blank">
+          NPM
+        </Links>
       </Sarmal>
     </>
   );
 };
-
 export default App;
